099773bd75364b056522fa3b4767fec88a2bbbb9
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-07-25 19:37:33 +0200

    Created API_call.py

---
+++ API_call.py
@@ -0,0 +1,12 @@
+from email.contentmanager import raw_data_manager
+import yfinance as yf
+
+raw_data = None
+
+def load_data(stock_list):
+    global raw_data
+    raw_data = yf.Ticker(stock_list)
+
+def get_raw_data():
+    global raw_data
+    return raw_data
